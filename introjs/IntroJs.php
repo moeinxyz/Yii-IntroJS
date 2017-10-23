b998927ccdf9adc07a90d324cf5929b7fc43a3a5
caitdeen <josh.rodriguez.b@gmail.com>
2017-10-23 15:24:52 -0500

    Added Hints render capabilities

--- introjs/IntroJs.php
+++ introjs/IntroJs.php
@@ -24,10 +24,20 @@ class IntroJs extends CWidget{
             foreach($this->data as $introData){
                 if (isset($introData['step']))
                     Yii::app()->clientScript->registerScript('introJs'.$introData['id'],"YiiIntroJS.set('".$introData['id']."',". $introData['step'] .",'". addslashes($introData['intro']) . "','" . addslashes($introData['position']) . "');");
+
+                if (isset($introData['hint']))
+                    Yii::app()->clientScript->registerScript('introJs'.$introData['id'],"YiiIntroJS.setHint('".$introData['id']."',". $introData['hint'] .",'". addslashes($introData['intro']) . "','" . addslashes($introData['position']) . "',". $introData['hintPosition'] .");");
+
                 if (isset($introData['start']) AND $introData['start'] == TRUE){
                     if (!isset($introData['event']))
                         $introData['event'] = 'onclick';
                     Yii::app()->clientScript->registerScript('introJs'.$introData['id'],"YiiIntroJS.start('". $introData['id'] . "','" . $introData['event'] ."');");}
+
+                 if (isset($introData['addHints']) AND $introData['addHints'] == TRUE){
+                    if (!isset($introData['event']))
+                        $introData['event'] = 'onclick';
+                    Yii::app()->clientScript->registerScript('introJs'.$introData['id'],"YiiIntroJS.addHints('". $introData['id'] . "','" . $introData['event'] ."');");}
+
                 if (isset($introData['exit']) AND $introData['exit'] == TRUE){
                     if (!isset($introData['event']))
                         $introData['event'] = 'onclick';
--- introjs/assets/js/YiiIntroJS.js
+++ introjs/assets/js/YiiIntroJS.js
@@ -3,6 +3,9 @@ var YiiIntroJS = (function(){
         start:function(selectorID,event){
             $("#"+selectorID).attr(event,"javascript:introJs().start();");
         },
+        addHints:function(selectorID,event){
+            $("#"+selectorID).attr(event,"javascript:introJs().addHints();");
+        },
         set:function(selectorID,step,intro,position){
             $("#"+selectorID).attr('data-step', step);
             $("#"+selectorID).attr('data-intro', intro);  
@@ -10,6 +13,14 @@ var YiiIntroJS = (function(){
                 $("#"+selectorID).attr('data-position', position);  
             else
                 $("#"+selectorID).attr('data-position', 'auto');  
+        },
+        setHint:function(selectorID,hint,intro,position,hintpos){
+            $("#"+selectorID).attr('data-hint', intro);
+            $("#"+selectorID).attr('data-hintposition', hintpos);  
+            if (position == 'right' || position == 'left'  || position == 'top' || position == 'bottom')
+                $("#"+selectorID).attr('data-position', position);  
+            else
+                $("#"+selectorID).attr('data-position', 'auto');
         },
         exit: function(selectorID,event){
             $("#"+selectorID).attr(event, "introJs().exit();");  
